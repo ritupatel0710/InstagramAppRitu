48dae907c0702bf375360315c23f9ca6196bc9bd
Ritu Patel <ritulimbani1234@gmail.com>
2018-08-01 20:44:39 -0500

    Initial Commit

---
+++ InstagramAppRitu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramAppRitu
+//
+//  Created by Ritu Patel on 8/1/18.
+//  Copyright © 2018 Ritu Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
